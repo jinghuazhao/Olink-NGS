d525ae89515062f949464c8abefeba1f6714ab67
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-01-04 17:58:26 +0000

    st.sh

---
+++ st.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add ngs.png ngs.do ngs.R ngs.sb ngs.sh lod.sh pQTLtools.R utils
+git commit -m "Olink/NGS pilot"
+git add st.sh
+git commit -m "st.sh"
+git push
