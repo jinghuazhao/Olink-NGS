09a39cccb4f948eaa934a6bc22680bf1ac2c1c95
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-01-20 17:09:45 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -6,6 +6,8 @@ git add README.md
 git commit -m "README"
 git add ngs.png ngs.do ngs.R ngs.sb ngs.sh lod.sh pQTLtools.R utils
 git commit -m "Olink/NGS pilot"
+git add Olink-NGS.md
+git commit -m "Olink/NGS"
 git add st.sh
 git commit -m "st.sh"
 git push
